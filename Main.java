c29e62f6c052e5eb16ffb2296a1ef26c12bda688
Tiago Xavier <tsxandmisuke2015@gmail.com>
2020-10-19 20:53:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+
+    TesteSimples teste = new TesteSimples();
+    teste.Executar();
+
+    System.out.println("imcmonitor, fim!");
+  }
+}
---
+++ TesteSimples.java
@@ -0,0 +1,12 @@
+class TesteSimples{
+
+  public void Executar(){
+
+    Atleta atleta = new Atleta();
+    atleta.nome = "Piedade Silva";
+    atleta.idade = 30;
+
+    System.out.println("Nome = " + atleta.nome);
+    System.out.println("Idade = " + atleta.idade);
+  }
+}
