a94e8becaf11f972135556bcc5a85d0b0cea60fa
Tiago Xavier <tsxandmisuke2015@gmail.com>
2020-10-19 22:04:14 +0000

    mudança de visibilidade de atributos de 'public' para 'private'

--- Atleta.java
+++ Atleta.java
@@ -1,7 +1,21 @@
 class Atleta {
 
-  String nome;
-  int idade;
+  private String nome;
+  private int idade;
 
-  
+  public void setNome(String n){
+    nome = n;
+  }
+
+  public void setIdade(int i){
+    idade = i;
+  }
+
+  public String getNome(){
+   return nome; 
+  }
+
+  public int getIdade(){
+   return idade; 
+  }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args){
 
     TesteSimples teste = new TesteSimples();
     teste.Executar();
